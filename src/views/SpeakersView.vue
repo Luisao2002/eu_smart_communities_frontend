2af3c5a11ec3772eccbcf836836b3c38b526f0a4
Luisao2002 <lu1sh3nriquerochalr@gmail.com>
2022-08-31 17:33:04 +0100

    oi

--- src/views/SpeakersView.vue
+++ src/views/SpeakersView.vue
@@ -13,7 +13,7 @@
             <MDBCardText>
               {{people.function}}
             </MDBCardText>
-            <button class="button button2">See Bio</button>
+            <!--<button id="myBtn" class="button button2">See Bio</button>-->
           </MDBCardBody>
         </MDBCard>
       </MDBCol>
@@ -49,20 +49,58 @@
         .then((data) => {
           this.people = data;
         });
-    }
+    },
   }
   </script>
   <style>
     .button2 {
-  background-color: #008CBA; 
+  background-color: #f0ed39; 
   color: white; 
-  border: 2px solid #008CBA;
+  border: 2px solid #f0ed39;
   border-radius: 5px;
 }
 
 .button2:hover {
-  background-color: #008CBA;
-  opacity: 70%;
-  color:white;
+  background-color: #f0ed39;
+  color:black;
+}
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
 }
   </style>
+
